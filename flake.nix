d4f956ed3aaacd68c641791463c202819443b19b
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-07-21 23:08:37 -0500

    feat: Add packages.default build output to flake
    Lets the crate be installed declaratively (e.g. from home-manager)
    by building the aoc binary straight from this repo, instead of
    relying on the nixpkgs-packaged version.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,19 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
+      packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "aoc-cli";
+        version = "0.12.2";
+        src = self;
+        cargoLock.lockFile = ./Cargo.lock;
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.openssl ];
+      };
+
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
           # rust
